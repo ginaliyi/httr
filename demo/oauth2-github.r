541274ca6cd538cab9edfe97384fa8ff0d55d7f6
ginaliyi <ginaliyi@users.noreply.github.com>
2014-12-12 11:08:21 +0800

    Update oauth2-github.r

--- demo/oauth2-github.r
+++ demo/oauth2-github.r
@@ -2,7 +2,7 @@ library(httr)
 
 # 1. Find OAuth settings for github:
 #    http://developer.github.com/v3/oauth/
-oauth_endpoints("github")
+github<- oauth_endpoints("github")
 
 # 2. Register an application at https://github.com/settings/applications;
 #    Use any URL you would like for the homepage URL (http://github.com is fine)
@@ -10,10 +10,10 @@ oauth_endpoints("github")
 #
 #    Insert your client ID and secret below - if secret is omitted, it will
 #    look it up in the GITHUB_CONSUMER_SECRET environmental variable.
-myapp <- oauth_app("github", key="88d5ba2e62da379649c4",secret="2f595d9f8abfefd945a09838120540d5973a1b56")
+myapp<- oauth_app("github", key="88d5ba2e62da379649c4",secret="2f595d9f8abfefd945a09838120540d5973a1b56")
 
 # 3. Get OAuth credentials
-github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+github_token <- oauth2.0_token(github, myapp)
 
 # 4. Use API
 gtoken <- config(token = github_token)
